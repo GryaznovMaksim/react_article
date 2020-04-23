8217506bb25013307c42d5c74f672a87dc9e750b
Maksim Gryaznov <nineval@gmail.com>
2020-04-23 18:13:56 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.3a12f7a1c3aa0162024865d01a660844.js"
 );
 
 self.addEventListener('message', (event) => {
